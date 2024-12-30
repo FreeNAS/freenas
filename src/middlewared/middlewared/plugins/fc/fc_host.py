8c2ee4d12aff8b332ca89fc4a47e0f31f05fc6f1
Brian M <brian.meagher@ixsystems.com>
2024-12-30 12:36:36 -0800

    Make fc.fc_host._validate more robust vs HA STANDBY unavailable

--- src/middlewared/middlewared/plugins/fc/fc_host.py
+++ src/middlewared/middlewared/plugins/fc/fc_host.py
@@ -79,7 +79,7 @@ class FCHostService(CRUDService):
         new = old.copy()
         new.update(data)
 
-        await self._validate("fc_host_update", new, id_, old)
+        await self._validate("fc_host_update", data, id_, old)
 
         await self.middleware.call(
             "datastore.update",
@@ -125,31 +125,46 @@ class FCHostService(CRUDService):
 
         if await self.middleware.call('failover.licensed'):
             # If failover is licensed then we allow both wwpn and wwpn_b to be set,
-            # but restricted to the available wwpns on each node
+            # but restricted to the available wwpns on each node.  However, we can
+            # only check this if both nodes are currently up ... so we'll only
+            # perform the check if the value is actually being changed.
+            node = None
             for key in ['alias', 'wwpn', 'wwpn_b']:
                 if data[key] is not None:
                     await self._ensure_unique(verrors, schema_name, key, data[key], id_)
-            if wwpn is not None or wwpn_b is not None:
-                node = await self.middleware.call('failover.node')
-                match node:
-                    case 'A':
-                        node_a_choices = await self.middleware.call('fc.fc_host_nport_wwpn_choices')
-                        node_b_choices = await self._get_remote_fc_host_nport_wwpn_choices()
-                    case 'B':
-                        node_a_choices = await self._get_remote_fc_host_nport_wwpn_choices()
-                        node_b_choices = await self.middleware.call('fc.fc_host_nport_wwpn_choices')
-                    case _:
-                        raise CallError('Cannot configure FC until HA is configured')
-                if wwpn and wwpn not in node_a_choices:
-                    verrors.add(
-                        f'{schema_name}.wwpn',
-                        f'Invalid wwpn ({wwpn}) supplied, should be one of: {",".join(node_a_choices)}'
-                    )
-                if wwpn_b and wwpn_b not in node_b_choices:
-                    verrors.add(
-                        f'{schema_name}.wwpn_b',
-                        f'Invalid wwpn ({wwpn_b}) supplied, should be one of: {",".join(node_b_choices)}'
-                    )
+
+            if wwpn is not None:
+                if old is None or old.get('wwpn') != wwpn:
+                    node = await self.middleware.call('failover.node')
+                    match node:
+                        case 'A':
+                            node_a_choices = await self.middleware.call('fc.fc_host_nport_wwpn_choices')
+                        case 'B':
+                            node_a_choices = await self._get_remote_fc_host_nport_wwpn_choices()
+                        case _:
+                            raise CallError('Cannot configure FC until HA is configured')
+                    if wwpn not in node_a_choices:
+                        verrors.add(
+                            f'{schema_name}.wwpn',
+                            f'Invalid wwpn ({wwpn}) supplied, should be one of: {",".join(node_a_choices)}'
+                        )
+
+            if wwpn_b is not None:
+                if old is None or old.get('wwpn_b') != wwpn_b:
+                    if not node:
+                        node = await self.middleware.call('failover.node')
+                    match node:
+                        case 'A':
+                            node_b_choices = await self._get_remote_fc_host_nport_wwpn_choices()
+                        case 'B':
+                            node_b_choices = await self.middleware.call('fc.fc_host_nport_wwpn_choices')
+                        case _:
+                            raise CallError('Cannot configure FC until HA is configured')
+                    if wwpn_b not in node_b_choices:
+                        verrors.add(
+                            f'{schema_name}.wwpn_b',
+                            f'Invalid wwpn ({wwpn_b}) supplied, should be one of: {",".join(node_b_choices)}'
+                        )
         else:
             for key in ['alias', 'wwpn']:
                 if data[key] is not None:
